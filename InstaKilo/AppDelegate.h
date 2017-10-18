b8443296938fe3fed3009391980883ca1650f87b
Aaron Johnson <aaron9johnson@gmail.com>
2017-10-18 11:08:07 -0700

    Initial Commit

---
+++ InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by Aaron Johnson on 2017-10-18.
+//  Copyright © 2017 Aaron Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
